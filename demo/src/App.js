7e393cf122da5f2cb33aecf508d4c55691b649a5
bgymn <58125826+14-TR@users.noreply.github.com>
2024-10-23 12:40:07 -0600

    commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Header from './components/Header';
 import ThemeProviderWrapper from './theme/ThemeProviderWrapper';
+import CustomDatePicker from './components/CustomDatePicker';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <div style={{ padding: 20 }}>
             <h1>Welcome to the App!</h1>
             <p>Toggle the theme using the button in the header.</p>
+            <CustomDatePicker />
           </div>
         </>
       )}
