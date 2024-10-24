6fd5e4b31666f19ffeae73e13b3ccaf941fbf0f7
bgymn <58125826+14-TR@users.noreply.github.com>
2024-10-24 12:44:20 -0600

    commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,13 +4,16 @@ import Header from './components/Header';
 import ThemeProviderWrapper from './theme/ThemeProviderWrapper';
 import CustomDatePicker from './components/CustomDatePicker';
 import PlaygroundSpeedDial  from './components/SpeedDial';
+import PrimarySearchAppBar from './components/AppBarSearch';
 
 function App() {
   return (
     <ThemeProviderWrapper>
       {({ toggleTheme, isDarkMode }) => (
         <>
-          <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+          {/* <Header toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+           */}
+          <PrimarySearchAppBar />
           <div style={{ padding: 20 }}>
             <h1>Welcome to the App!</h1>
             <p>Toggle the theme using the button in the header.</p>
